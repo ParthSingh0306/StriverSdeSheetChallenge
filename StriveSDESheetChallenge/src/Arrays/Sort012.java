df5ea8dc49fb1c2930eb630f24a1cb768ed1a667
Parth Singh <parth3743@gmail.com>
2023-06-04 16:17:03 +0530

    First Commit.

---
+++ StriveSDESheetChallenge/src/Arrays/Sort012.java
@@ -0,0 +1,42 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Sort012 {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 0, 0, 2};
+		System.out.println(Arrays.toString(arr));
+		sort012(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	public static void sort012(int[] arr){
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        int mid = 0;
+
+        while(mid <= high) {
+            if(arr[mid] == 0) {
+                swap(low, mid, arr);
+                mid++;
+                low++;
+            }
+            else if(arr[mid] == 2) {
+                swap(high, mid, arr);
+                high--;
+            }
+            else {
+                mid++;
+            }
+        }
+    }
+
+    public static void swap(int f, int s, int[] arr) {
+        int temp = arr[f];
+        arr[f] = arr[s];
+        arr[s] = temp;
+    }
+
+}
